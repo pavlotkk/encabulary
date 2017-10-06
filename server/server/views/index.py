e006a878b5f75b4f82008545921bfb7c5faa9897
Pavel Tkachuk <tkachuk.pavel13@gmail.com>
2017-10-07 01:25:52 +0300

    redirect authorized user from index to learn page

--- server/server/views/index.py
+++ server/server/views/index.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import render_template, redirect
 from flask.views import MethodView
+
+from server.api.base.request import get_current_user_id
 
 
 class IndexView(MethodView):
     def get(self):
-        return render_template('index.html')
+        user_id = get_current_user_id()
+
+        if not user_id:
+            return render_template('index.html')
+
+        return redirect('/learn')
