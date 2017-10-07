f4fa3d6b0859d285ed4f565f318c67175a68d655
Pavel Tkachuk <tkachuk.pavel13@gmail.com>
2017-10-08 00:34:27 +0300

    add reverso scrapper

--- server/server/api/dictionary/autocomplete.py
+++ server/server/api/dictionary/autocomplete.py
@@ -3,6 +3,7 @@ from flask.views import MethodView
 from server.api.base.response import ok_response
 from server.autocomplete.oxford_dictionary import OxfordDictionary
 from server.autocomplete.linguee import LingueeDictionary
+from server.autocomplete.reverso import ReversoDictionary
 from server.database.model import DbWordType
 
 
@@ -18,9 +19,13 @@ class AutoCompleteAPI(MethodView):
             id_word_type = DbWordType.get_id_by_name(oxford_item.word_type_name)
 
         linguee_dictionary = LingueeDictionary()
-        linguee_items = linguee_dictionary.get_items(word)
 
-        translations = [item.translations for item in linguee_items if
+        translation_items = linguee_dictionary.get_items(word)
+        if not translation_items:
+            reverso_dictionary = ReversoDictionary()
+            translation_items = reverso_dictionary.get_items(word)
+
+        translations = [item.translations for item in translation_items if
                         DbWordType.get_id_by_name(item.word_type_name) == id_word_type]
 
         if translations:
