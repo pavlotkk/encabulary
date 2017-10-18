eab7bf3c7848d69942d615e1901fc51becf2adeb
Pavel Tkachuk <tkachuk.pavel13@gmail.com>
2017-10-18 23:58:21 +0300

    fix logout

--- server/server/api/auth/logout.py
+++ server/server/api/auth/logout.py
@@ -1,6 +1,7 @@
+from flask import redirect
 from flask.views import MethodView
 
-from server.api.base.request import get_current_user_id
+from server.api.base.request import get_current_user_id, get_current_request
 from server.api.base.response import ok_response
 from server.database.management.db_manager import save_db_changes
 from server.database.queries import get_db_user_by_id
@@ -8,7 +9,6 @@ from server.decorators.access_token_required import AccessTokenRequired
 
 
 class LogoutAPI(MethodView):
-
     @AccessTokenRequired()
     def post(self):
         current_user_id = get_current_user_id()
@@ -16,7 +16,12 @@ class LogoutAPI(MethodView):
         db_user = get_db_user_by_id(current_user_id)
         self._remove_user_session_and_access_token(db_user)
 
-        response = ok_response()
+        request = get_current_request()
+        if request.http_request.args.get('redirect', 'false') == 'true':
+            response = redirect('/index')
+        else:
+            response = ok_response()
+
         response.set_cookie('access_token', '', expires=0)
 
         return response
