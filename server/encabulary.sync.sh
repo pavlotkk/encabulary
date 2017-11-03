e3a96bbc7aad447dfa68801d5ef99efc78e9c8f4
Pavel Tkachuk <tkachuk.pavel13@gmail.com>
2017-11-03 15:38:36 +0200

    add syncignore file

---
+++ server/encabulary.sync.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rsync -rhv --exclude-from .syncerignore ./ /usr/share/nginx/www/encabulary
+rsync -rhv server/encabulary.supervisor.conf /etc/supervisor/conf.d/
+rsync -rhv server/encabulary.uwsgi.ini /etc/uwsgi/
+rsync -rhv server/encabulary.nginx.conf /etc/nginx/sites-available/
+ln -sf /etc/nginx/sites-available/encabulary.nginx.conf /etc/nginx/sites-enabled
+
+chown -R www-data /usr/share/nginx/www/encabulary
+
+cd /usr/share/nginx/www/encabulary
+python3 -m virtualenv env
+env/bin/pip install -r requirements.txt
+
+sudo service nginx restart
+sudo supervisorctl update
+sudo supervisorctl restart encabulary
